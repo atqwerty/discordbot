d30d515d79c5c59fc8ec4ce55de688494f39f388
Markitanov Denis <dmarkitanov@gmail.com>
2019-03-23 18:30:10 +0600

    testing ping reply

--- my_bot.js
+++ my_bot.js
@@ -2,28 +2,15 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
-
- 
 
 client.on('message', message => {
-
-    if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+    if (message.content.includes("!ping")) {
+        user = message.mentions.users.first();
+        message.channel.send('look over here ' + user);
+    }
 });
-
- 
-
 // THIS  MUST  BE  THIS  WAY
-
 client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
