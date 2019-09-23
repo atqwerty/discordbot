c402bd47d6a42bde0a469ea23c7e6006890a2b5e
Denis <dmarkitanov@gmail.com>
2019-09-23 20:27:24 +0600

    added rm 'HW with multivariant and normal equation.docx'  command for retards

--- my_bot.js
+++ my_bot.js
@@ -1,19 +1,25 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 
-const client = new Discord.Client();
+const client = new Discord.Client()
 
 client.on('ready', () => {
-    console.log('I am ready!');
-});
+  console.log('I am ready!')
+})
 
 client.on('message', message => {
-    if (message.content.includes("!ping")) {
-        var i;
-        user = message.mentions.users.first();
-        for(i = 0; i < 10; i++){
-            message.channel.send('look over here ' + user);
-        }
+  if (message.content.includes('!ping')) {
+    var i
+    const user = message.mentions.users.first()
+    for (i = 0; i < 10; i++) {
+      message.channel.send('look over here ' + user)
     }
-});
+  } else if (message.content.includes('!r')) {
+    var i
+    for (i = 0; i < 18; i++) {
+      message.channel.send('$m')
+    }
+  }
+})
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+// BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN)
